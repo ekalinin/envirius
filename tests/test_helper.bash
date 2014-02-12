1e60762f7792d22bcac6ca099a057c3577f240ff
Eugene Kalinin <e.v.kalinin@gmail.com>
2014-02-12 23:17:16 +0400

    added some helpers for tests

--- tests/test_helper.bash
+++ tests/test_helper.bash
@@ -1,6 +1,6 @@
 setup() {
     export MEM_PATH="$BATS_TEST_DIRNAME/../src"
-    export MEM_HOME="$BATS_TEST_DIRNAME/tmp-mem-home"
+    export MEM_HOME="$HOME/tmp/mem-tests-home"
     mkdir -p $MEM_HOME
     . $BATS_TEST_DIRNAME/../src/mem
 }
@@ -10,5 +10,18 @@ assert_equal() {
     echo "expected: $1"
     echo "actual:   $2"
     return 1
+  fi
+}
+
+assert_output() {
+    assert_equal "$1" "$output"
+}
+
+assert_success() {
+  if [ "$status" -ne 0 ]; then
+    echo "command failed with exit status $status"
+    return 1
+  elif [ "$#" -gt 0 ]; then
+    assert_output "$1"
   fi
 }
